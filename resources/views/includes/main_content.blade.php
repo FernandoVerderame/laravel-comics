a6a5978e133a8cb89eef1b19adeb2bf2df981e43
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-03-07 17:52:53 +0100

    added template main content

---
+++ resources/views/includes/main_content.blade.php
@@ -0,0 +1,20 @@
+<main>
+    <section id="comics">
+        <div class="container">
+            <h2>Current Series</h2>
+            <div class="card-container">
+                <div class="comic-card">
+                    <div class="thumb">
+                        <img src="" alt="">
+                    </div>
+                    <h3></h3>
+                </div>
+            </div>
+        </div>
+        <div class="lead-more">
+            <div class="button">
+                <a href="#">Lead More</a>
+            </div>
+        </div>
+    </section>
+</main>
