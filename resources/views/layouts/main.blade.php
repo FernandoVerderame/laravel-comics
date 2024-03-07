adadc703b9c90d4c2c8e9184e0c8b28e49cbe229
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-03-07 15:21:11 +0100

    added icon & title

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -4,10 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Main</title>
+    <title> {{ env('APP_NAME', 'DC Comics') }} | @yield('title')</title>
+
+    <link rel="icon" href="{{ Vite::asset('resources/img/favicon.ico') }}" type="images/ico">
 
     {{-- Import Vite & co --}}
     @vite('resources/js/app.js')
+
+    {{-- CDNS --}}
+    @yield('cdns')
 
 </head>
 <body>
