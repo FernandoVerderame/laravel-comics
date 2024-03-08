06b83f9d06540036285a6d5a3e329de7d0393f16
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-03-08 09:54:08 +0100

    added copyright

--- resources/views/includes/footer_top.blade.php
+++ resources/views/includes/footer_top.blade.php
@@ -1,21 +1,43 @@
 <div class="footer-top">
     <div class="bg-dc">
-        <div class="links container">
-            <div class="left-links">
-                <div class="dc-comics-links">
-                    <h4>DC COMICS</h4>
+        <div class="container">
+            <div class="links">
+                <div class="left-links">
+                    <div class="dc-comics-links">
+                        <h4>DC COMICS</h4>
+                        <ul>
+                            @foreach(config('dc_comics_links') as $link)
+                            <li>
+                                <a href="#" class="">{{ $link['text'] }}</a>
+                            </li>
+                            @endforeach
+                        </ul>
+                    </div>
+                    <div class="shop-links">
+                        <h4>SHOP</h4>
+                        <ul>
+                            @foreach(config('shop_links') as $link)
+                            <li>
+                                <a href="#" class="">{{ $link['text'] }}</a>
+                            </li>
+                            @endforeach
+                        </ul>
+                    </div>
+                </div>
+                <div class="dc-links">
+                    <h4>DC</h4>
                     <ul>
-                        @foreach(config('dc_comics_links') as $link)
+                        @foreach(config('dc_links') as $link)
                         <li>
                             <a href="#" class="">{{ $link['text'] }}</a>
                         </li>
                         @endforeach
                     </ul>
                 </div>
-                <div class="shop-links">
-                    <h4>SHOP</h4>
+                <div class="sites-links">
+                    <h4>SITES</h4>
                     <ul>
-                        @foreach(config('shop_links') as $link)
+                        @foreach(config('sites_links') as $link)
                         <li>
                             <a href="#" class="">{{ $link['text'] }}</a>
                         </li>
@@ -23,25 +45,8 @@
                     </ul>
                 </div>
             </div>
-            <div class="dc-links">
-                <h4>DC</h4>
-                <ul>
-                    @foreach(config('dc_links') as $link)
-                    <li>
-                        <a href="#" class="">{{ $link['text'] }}</a>
-                    </li>
-                    @endforeach
-                </ul>
-            </div>
-            <div class="sites-links">
-                <h4>SITES</h4>
-                <ul>
-                    @foreach(config('sites_links') as $link)
-                    <li>
-                        <a href="#" class="">{{ $link['text'] }}</a>
-                    </li>
-                    @endforeach
-                </ul>
+            <div class="copyright">All Site Content TM and &#169; 2020 DC Entertainment, unless otherwise <a href="#">noted here</a>. All rights reserved. <br>
+                <a href="#">Cookies Settings</a>
             </div>
         </div>
     </div>
